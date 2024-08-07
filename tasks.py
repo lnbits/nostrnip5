c36fe3d8a68b6116071f1190d4546370234df466
Vlad Stan <stan.v.vlad@gmail.com>
2024-08-07 10:45:48 +0300

    refactor: smaller methods

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ from lnbits.tasks import register_invoice_listener
 from loguru import logger
 
 from .crud import get_address, update_address
-from .models import LnAddressConfig
+from .models import Address
 from .services import activate_address, update_ln_address
 
 
@@ -33,32 +33,65 @@ async def on_invoice_paid(payment: Payment) -> None:
         )
         return
 
-    address = await get_address(domain_id, address_id)
-    if not address:
-        logger.info(
-            f"Cannot find address for payment '{payment.payment_hash}'."
-            f"Missing domain ID ({domain_id}) or address ID ({address_id})."
+    try:
+        address = await get_address(domain_id, address_id)
+        if not address:
+            logger.info(
+                f"Cannot find address for payment '{payment.payment_hash}'."
+                f"Missing domain ID ({domain_id}) or address ID ({address_id})."
+            )
+            return
+
+        await _handle_action(action, payment, address)
+    except Exception as exc:
+        logger.warning(exc)
+        logger.info(f"Failed to {action} address `{domain_id}/{address_id}`")
+
+
+async def _handle_action(action: str, payment: Payment, address: Address):
+    if action == "activate":
+        await _activate_address(payment, address)
+    if action == "reimburse":
+        await _reimburse_payment(address)
+
+
+async def _activate_address(payment: Payment, address: Address):
+    activated = await activate_address(
+        address.domain_id, address.id, payment.payment_hash
+    )
+    if activated:
+        await _create_ln_address(payment, address)
+    else:
+        await _update_reimburse_data(payment, address)
+
+
+async def _update_reimburse_data(payment: Payment, address: Address):
+    address.config.reimburse_payment_hash = payment.payment_hash
+    await update_address(
+        address.domain_id,
+        address.id,
+        reimburse_amount=payment.amount,
+        config=address.config,
+    )
+
+
+async def _create_ln_address(payment: Payment, address: Address):
+    wallet = payment.extra.get("reimburse_wallet_id")
+    if not wallet:
+        logger.warning(
+            "Now wallet found for Lightning Address"
+            f" '{address.local_part} ({address.id}')."
         )
         return
+    address.config.ln_address.wallet = wallet
+    pay_link_data = await update_ln_address(
+        address.local_part, address.config.ln_address
+    )
+    address.config.ln_address = pay_link_data
+    logger.success(f"Updated Lightning Address for '{address.id}'.")
 
-    if action == "activate":
-        activated = await activate_address(domain_id, address.id, payment.payment_hash)
-        if activated:
-            wallet = payment.extra.get("reimburse_wallet_id")
-            config = LnAddressConfig(wallet=wallet)
-            pay_link_data = await update_ln_address(address.local_part, config)
-            config.ln_address = pay_link_data
-            logger.success(f"Updated Lightning Address for '{address.id}'.")
+    await update_address(address.domain_id, address.id, config=address.config)
 
-            await update_address(address.domain_id, address.id, config=config)
 
-        else:
-            address.config.reimburse_payment_hash = payment.payment_hash
-            await update_address(
-                address.domain_id,
-                address.id,
-                reimburse_amount=payment.amount,
-                config=address.config,
-            )
-    elif action == "reimburse":
-        await update_address(address.domain_id, address.id, reimburse_amount=0)
+async def _reimburse_payment(address: Address):
+    await update_address(address.domain_id, address.id, reimburse_amount=0)
